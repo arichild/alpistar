45429a49dacad4a0946d2d3e8605040bf60d8ceb
arichild <arieevq@gmail.com>
2025-08-20 19:58:42 +0300

    Save

--- dist/js/main.js
+++ dist/js/main.js
@@ -93,4 +93,128 @@ $( document ).ready(function() {
       })
     });
   }
+
+  // свойство left для блока в карте
+  const mapBlock = document.querySelector('.map-info')
+
+  if(mapBlock) {
+    function setLeftPoint() {
+      if(window.innerWidth > 1024) {
+        const footer = document.querySelector('.footer-content')
+        const leftPoint = footer.getBoundingClientRect().left
+
+        mapBlock.style.left = leftPoint + 'px'
+      } else {
+        mapBlock.style.left = ''
+      }
+    }
+    setLeftPoint()
+
+    window.addEventListener('resize', () => {
+      setLeftPoint()
+    })
+  }
+
+  // splide
+  const splideSpecialists = document.querySelector('.splide-specialists')
+  const splideMain = document.querySelector('.splide-main')
+  const splideAbout = document.querySelector('.splide-about')
+
+  if(splideSpecialists) {
+    new Splide( splideSpecialists, {
+      destroy: true,
+      perPage: 2,
+      gap: 20,
+      arrows: false,
+
+      breakpoints: {
+        768: {
+          destroy: false
+        },
+
+        576: {
+          perPage: 1,
+        }
+      }
+    }).mount();
+  }
+
+  if(splideMain) {
+    const allSliders = splideMain.querySelectorAll('.splide__slide').length
+
+    if(allSliders > 1) {
+      new Splide( splideMain, {
+        type: 'loop',
+        autoplay: true,
+        perPage: 1,
+        pagination: false,
+        arrows: false,
+      }).mount()
+    } else {
+      new Splide( splideMain, {
+        pagination: false,
+        arrows: false,
+        drag: false
+      }).mount()
+    }
+  }
+
+  if(splideAbout) {
+    new Splide( '.splide', {
+      pagination: false,
+      arrows: false,
+      autoWidth: true,
+      gap: 10,
+      drag: true,
+      type: 'loop',
+
+      breakpoints: {
+        1024: {
+          height: 350
+        },
+        768: {
+          height: 300
+        },
+        576: {
+          height: 220,
+        },
+      }
+    }).mount();
+  }
+
+  // burger
+  const burgerBtn = document.querySelector('.header-burger')
+
+  if(burgerBtn) {
+    const burger = document.querySelector('.header-mob')
+    const html = document.documentElement
+
+    burgerBtn.addEventListener('click', () => {
+      burger.classList.toggle('active')
+      html.classList.toggle('active')
+      burgerBtn.classList.toggle('active')
+    })
+  }
+
+  // hint
+  // const aboutBlock = document.querySelector('.about-slider')
+
+  // if(aboutBlock) {
+  //   const dragHint = document.querySelector('.drag-hint')
+  //   aboutBlock.addEventListener('mousemove', (event) => {
+  //     const x = event.clientX;
+  //     const y = event.clientY;
+
+  //     console.log(x)
+  //     console.log(y)
+
+  //     //  dragHint.style.left = `${x}px`;
+  //     // dragHint.style.top = `${y}px`;
+  //     dragHint.style.transform = `translate(${x}px, ${y}px)`;
+  //     dragHint.classList.add('active')
+  //   });
+  //   aboutBlock.addEventListener('mouseout', (event) => {
+  //     dragHint.classList.remove('active')
+  //   });
+  // }
 });
